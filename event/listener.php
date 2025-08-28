ac6ee706032f548b9de7649b791f3777fe0455b3
Sylver35 <33929219+Sylver35@users.noreply.github.com>
2025-08-28 14:27:36 +0200

    Add files via upload

--- event/listener.php
+++ event/listener.php
@@ -82,13 +82,10 @@ class listener implements EventSubscriberInterface
 	public function parse_bbcodes_after($event)
 	{
 		$text = (string) $event['text'];
-		if ($text !== null)
+		if (strpos($text, 'S_CREATOR_BBCODE') !== false)
 		{
-			if (strpos($text, 'S_CREATOR_BBCODE') !== false)
-			{
-				$app = (!$this->config['enable_mod_rewrite']) ? '/app.' . $this->php_ext : '';
-				$event['text'] = str_replace('%7CS_CREATOR_BBCODE%7Capp.php', generate_board_url() . $app, $text);
-			}
+			$app = (!$this->config['enable_mod_rewrite']) ? '/app.' . $this->php_ext : '';
+			$event['text'] = str_replace('%7CS_CREATOR_BBCODE%7Capp.php', generate_board_url() . $app, $text);
 		}
 	}
 
